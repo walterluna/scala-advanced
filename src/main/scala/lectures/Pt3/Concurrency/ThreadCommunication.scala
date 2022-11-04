f7b8819ee3b4ad5e810d99bd3f441197f8511346
Walter Luna <mywalter97@gmail.com>
2022-11-04 17:38:38 -0600

    Producer/Consumer pt2

--- src/main/scala/lectures/Pt3/Concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/Pt3/Concurrency/ThreadCommunication.scala
@@ -1,5 +1,8 @@
 package lectures.Pt3.Concurrency
 
+import scala.collection.mutable
+import scala.util.Random
+
 object ThreadCommunication extends App {
 
   /* Producer-Consumer problem
@@ -77,5 +80,58 @@ object ThreadCommunication extends App {
   }
 
   //  naiveProdCons()
-  smartProdCons()
+  //  smartProdCons()
+
+  /*   Buffer of values
+  producer -> [ ? ? ? ] -> consumer
+
+   */
+
+  def prodConsLargeBuffer(): Unit = {
+    val buffer: mutable.Queue[Int] = new mutable.Queue[Int]
+    val capacity = 3
+
+    val consumer = new Thread(() => {
+      val random = new Random()
+
+      while (true) {
+        buffer.synchronized {
+          if (buffer.isEmpty) {
+            println("[consumer] buffer empty, waiting")
+            buffer.wait()
+          }
+
+          val x = buffer.dequeue()
+          println("[consumer] I consumed: " + x)
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    })
+
+    val producer = new Thread(() => {
+      val random = new Random()
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+          if (buffer.size == capacity) {
+            println("[producer] buffer is full, waiting")
+            buffer.wait()
+          }
+
+          println("[producer] producing: " + i)
+          buffer.enqueue(i)
+          i += 1
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    })
+
+    consumer.start()
+    producer.start()
+  }
+
+  prodConsLargeBuffer()
 }
