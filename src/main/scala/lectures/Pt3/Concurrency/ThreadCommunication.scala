f86a5c4887b38b96fce12ec55ba166c6f6fee584
Walter Luna <mywalter97@gmail.com>
2022-11-04 18:11:48 -0600

    Producer/Consumer pt3

--- src/main/scala/lectures/Pt3/Concurrency/ThreadCommunication.scala
+++ src/main/scala/lectures/Pt3/Concurrency/ThreadCommunication.scala
@@ -133,5 +133,63 @@ object ThreadCommunication extends App {
     producer.start()
   }
 
-  prodConsLargeBuffer()
+  //  prodConsLargeBuffer()
+
+  /* Prod-cons, level 3
+  producer1 -> [ ? ? ? ] -> consumer2
+  producer2 -> ^^^^^^^^  -> consumer2
+   */
+
+  class Consumer(id: Int, buffer: mutable.Queue[Int]) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+
+      while (true) {
+        buffer.synchronized {
+          while (buffer.isEmpty) {
+            println(s"[consumer $id] buffer empty, waiting")
+            buffer.wait()
+          }
+
+          val x = buffer.dequeue()
+          println(s"[consumer $id] I consumed: " + x)
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+    }
+  }
+
+  class Producer(id: Int, buffer: mutable.Queue[Int], capacity: Int) extends Thread {
+    override def run(): Unit = {
+      val random = new Random()
+      var i = 0
+
+      while (true) {
+        buffer.synchronized {
+          while (buffer.size == capacity) {
+            println(s"[producer $id] buffer is full, waiting")
+            buffer.wait()
+          }
+
+          println(s"[producer $id] producing: " + i)
+          buffer.enqueue(i)
+          i += 1
+          buffer.notify()
+        }
+        Thread.sleep(random.nextInt(500))
+      }
+
+    }
+  }
+
+  def multiProdCons(nConsumers: Int, nProducers: Int): Unit = {
+    val buffer = new mutable.Queue[Int]()
+    val capacity = 3
+
+    (1 to nConsumers).foreach(new Consumer(_, buffer).start())
+    (1 to nProducers).foreach(new Producer(_, buffer, capacity).start())
+  }
+
+  multiProdCons(5, 5)
 }
