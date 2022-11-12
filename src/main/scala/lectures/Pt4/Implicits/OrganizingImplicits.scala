14f0e19fc51b2c68f2481097d8546295ad85b1b4
Walter Luna <mywalter97@gmail.com>
2022-11-11 19:10:33 -0600

    Organizing implicits

---
+++ src/main/scala/lectures/Pt4/Implicits/OrganizingImplicits.scala
@@ -0,0 +1,76 @@
+package lectures.Pt4.Implicits
+
+object OrganizingImplicits extends App {
+  implicit val reverseOrdering: Ordering[Int] = Ordering.fromLessThan(_ > _)
+  //  implicit val normalOrdering: Ordering[Int] = Ordering.fromLessThan(_ < _)
+
+  println(List(1, 4, 3, 2, 5).sorted)
+
+  //  imported from scala.predef
+
+  /*
+  Implicits (used as implicit params):
+    -val/var
+    -object
+    -accessor methods (def with no parentheses)
+   */
+
+  /* Exercise: Create Person ordering
+   */
+  case class Person(name: String, age: Int)
+
+  val persons = List(
+    Person("Steve", 30),
+    Person("Amy", 22),
+    Person("John", 66),
+  )
+
+  //  object Person {
+  //    implicit val alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0)
+  //  }
+  //  implicit val ageOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.age < b.age)
+  //  println(persons.sorted)
+
+  /*
+    Implicit scope
+    - normal scope = LOCAL SCOPE
+    - imported scope
+    - companions of all types involved in the method signature
+      - List
+      - Ordering
+      - all the types involved = A or any supertype
+   */
+  // def sorted[B >: A](implicit ord: Ordering[B]): List[B]
+
+  object AlphabeticNameOrdering {
+    implicit val alphabeticOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.name.compareTo(b.name) < 0)
+  }
+
+  object AgeOrdering {
+    implicit val ageOrdering: Ordering[Person] = Ordering.fromLessThan((a, b) => a.age < b.age)
+  }
+
+  import AgeOrdering._
+
+  println(persons.sorted)
+
+  /*
+    Exercise.
+    - totalPrice = most used (50%)
+    - by unit count = 25%
+    - by unit price = 25%
+   */
+  case class Purchase(nUnits: Int, unitPrice: Double)
+
+  object Purchase {
+    implicit val totalPriceOrdering: Ordering[Purchase] = Ordering.fromLessThan((a, b) => a.nUnits * a.unitPrice < b.nUnits * b.unitPrice)
+  }
+
+  object UnitCountOrdering {
+    implicit val unitCountOrdering: Ordering[Purchase] = Ordering.fromLessThan(_.nUnits < _.nUnits)
+  }
+
+  object UnitPriceOrdering {
+    implicit val unitPriceOrdering: Ordering[Purchase] = Ordering.fromLessThan(_.unitPrice < _.unitPrice)
+  }
+}
